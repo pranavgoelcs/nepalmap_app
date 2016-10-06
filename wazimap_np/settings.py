2f87a155071f2bf2c6c4407c34b358554d92d145
Prashish Rajbhandari <prashishh@gmail.com>
2016-10-05 21:53:57 -0700

    Added a Procfile & test config.

--- wazimap_np/settings.py
+++ wazimap_np/settings.py
@@ -5,7 +5,7 @@ from wazimap.settings import *  # noqa
 INSTALLED_APPS = ['wazimap_np'] + INSTALLED_APPS
 
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://wazimap_np:wazimap_np@localhost/wazimap_np')
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://wazimap_np:wazimap_np@localhost/postgres')
 DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
